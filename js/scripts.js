a7f3603d86e5f0f77fda98e0db881a799c8cc947
Andy Priebe <priebea@comcast.net>
2015-10-09 11:53:02 -0700

    Corrected code to index to put all functions withing container class.  Updated js to include script for hiding and showing image of Grumpy Cat but this function and printed list still not working

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var favorite1Input = $("input#favorite1").val();
+    var favorite2Input = $("input#favorite2").val();
+    var favorite3Input = $("input#favorite3").val();
+    var favorite14nput = $("input#favorite4").val();
+    var favorite5Input = $("input#favorite5").val();
+
+    $(".favorite1").text(favorite1Input);
+    $(".favorite2").text(favorite2Input);
+    $(".favorite3").text(favorite3Input);
+    $(".favorite4").text(favorite4Input);
+    $(".favorite5").text(favorite5Input);
+
+    $("#list").show();
+
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#grumpy-showing").toggle();
+    $("#grumpy-hidden").toggle();
+  });
+});
